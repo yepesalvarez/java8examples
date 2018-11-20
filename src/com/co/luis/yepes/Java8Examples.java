c55ab9335dc0447505ee3ac1dcfa2734658904ae
yepesalvarez <yepesluisfernando@hotmail.com>
2018-11-19 19:34:39 -0500

    last tests and main class implementations

--- src/com/co/luis/yepes/Java8Examples.java
+++ src/com/co/luis/yepes/Java8Examples.java
@@ -1,25 +1,33 @@
 package com.co.luis.yepes;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+import org.apache.commons.lang3.math.NumberUtils;
 
 public class Java8Examples {
 	
 // 1. Return the sum from the numbers among a given rank ----------------------------------------
 	public int numbersBetweenRank(int initialRank, int finalRank) {
-			return IntStream
+		return
+				IntStream
 					.rangeClosed(initialRank, finalRank)
 					.sum();
 	}
-// 2. Return the sum from a given double numbers array using reduce ----------------------------
+// 2. Return the sum from a given double numbers array using reduce -----------------------------
 	public double reduceSum(double[] numbers) {
 		if(numbers != null)
 			return Arrays.stream(numbers)
-				.reduce(0.0, (a, b) -> a + b);
+					.reduce(0.0, (a, b) -> a + b);
 		throw new NullPointerException();
 	}
 // 3. Return the average from the numbers among a given rank, skip the n first elements ---------
@@ -30,7 +38,7 @@ public class Java8Examples {
 					.skip(n)
 					.average()
 					.getAsDouble();
-		throw new NullPointerException();
+		throw new NoSuchElementException();
 	}
 // 4. Order and return the first element of a given words list ----------------------------------
 	public String firstOrderedWord(List<String> words) {
@@ -41,7 +49,7 @@ public class Java8Examples {
 					.orElse(null);
 		throw new NullPointerException();
 	}
-// 5. Return the bigger square from a given decimal numbers array ------------------------------
+// 5. Return the bigger square from a given decimal numbers array -------------------------------
 	public double biggerDecimalSquare(Double[] numbers) {
 		if(numbers != null){
 			if(numbers.length > 0) {
@@ -54,30 +62,87 @@ public class Java8Examples {
 	}
 // 6.  Filter by prefix, order and return the words array changed from the given one ------------
 	public String[] orderedFilteredWords(String[] words, String prefix) {
-		return words;
+		if(words!=null && prefix!=null)
+			return
+				Arrays.stream(words)
+					.filter(x -> x.startsWith(prefix))
+					.sorted(String::compareToIgnoreCase)
+					.toArray(String[]::new);
+		throw new NullPointerException();
 	}
 // 7. Return the amount of words with more than four letters in a referenced text file ----------
-	public int biggerThan4(String uriTxt) {
-		return 0;
+	public int biggerThan4(String uriTxt) throws IOException {
+		if(uriTxt != null) {
+			Stream<String> words = Files.lines(Paths.get(uriTxt));
+			int wordsBiggerThan4 =   
+				(int) words
+						.map(x -> x.split(" "))
+						.flatMap(Arrays::stream)
+						.filter(x -> x.length() > 4)
+						.count();
+			words.close();
+			return wordsBiggerThan4;
+		}else {throw new NullPointerException();}
 	}
-/* 8. Return from a referenced text file, in uppercase,------------------------------------------
- * 	the list of words that end with a given postfix ---------------------------------------------
+/* 8. Return from a referenced text file, in upper case,------------------------------------------
+ * 	the list of words that end with a given suffix ---------------------------------------------
  */
-	public List<String> endWithPostfix(String uriTxt, String postfix){
-		return null;
+	public List<String> endWithSuffix(String uriTxt, String suffix) throws IOException{
+		if(uriTxt != null) {
+			Stream<String> words = Files.lines(Paths.get(uriTxt));
+			List<String> wordsSuffix =
+				words
+					.map(x -> x.split(" "))
+					.flatMap(Arrays::stream)
+					.filter(x -> x.endsWith(suffix))
+					.map(String::toUpperCase)
+					.collect(Collectors.toList());
+			words.close();
+			return wordsSuffix;
+		}else {throw new NullPointerException();}
 	}
 // 9. Return how many rows with numbers does a referenced CSV file has --------------------------
-	public int rowsWithNumbers(String uriCsv) {
-		return 0;	
+	public int rowsWithNumbers(String uriCsv) throws IOException {
+		if(uriCsv != null) {
+			Stream<String> words = Files.lines(Paths.get(uriCsv));
+			int rows = 
+					(int) words
+							.map(x -> x.split(","))
+							.filter( x -> Stream.of(x)
+											.map(z -> z.split(""))
+											.flatMap(Arrays::stream)
+											.filter(NumberUtils::isCreatable)
+											.count() > 0)
+							.count();
+			words.close();
+			return rows;
+		}else {throw new NullPointerException();}
 	}
 // 10. Return an array with all prime numbers from a referenced CSV file ------------------------
-	public int[] primeNumbers(String uriCsv) {
-		return null;
+	public int[] primeNumbers(String uriCsv) throws IOException {
+		if(uriCsv!=null) {
+			Stream<String> numbers = Files.lines(Paths.get(uriCsv));
+			int[] primeNumbers = numbers
+					.map(x -> x.split(","))
+					.flatMap(Arrays::stream)
+					.map(x -> x.replace(" ", ""))
+					.filter(NumberUtils::isCreatable)
+					.filter(x -> IntStream
+							.range(2, NumberUtils.createInteger(x))
+							.noneMatch(i -> NumberUtils.createInteger(x) % i == 0))
+					.mapToInt(NumberUtils::createInteger)
+					.toArray();
+			numbers.close();
+			return primeNumbers;
+		}else {throw new NullPointerException();}
 	}
 /* 11. Return a String with the IntSummaryStatistics from the people's ages in a given Person ---
  * list. Person class should have id, name, age properties. Skip those with repeated id --------- 
  */
 	public String ageStatistics(List<Person> people) {
-		return null;
+		if (people != null) {
+			Collection<Person> noRepeated = people.stream().collect(Collectors.toMap(Person::getId, p -> p, (p, q) -> p)).values();
+			return noRepeated.stream().collect(Collectors.summarizingInt(Person::getAge)).toString();
+		}else {throw new NullPointerException();}
 	}
 }
--- testsrc/com/co/luis/yepes/Java8ExamplesTest.java
+++ testsrc/com/co/luis/yepes/Java8ExamplesTest.java
@@ -2,6 +2,7 @@ package com.co.luis.yepes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -35,6 +36,21 @@ class Java8ExamplesTest {
 		double actual = java8Examples.reduceSum(numbers);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	void testReduceSumNullArray() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.reduceSum(null);
+		});
+	}
+	
+	@Test
+	void testReduceSumEmptyArray() {
+		double[] numbers = {};
+		double expected = 0;
+		double actual = java8Examples.reduceSum(numbers);
+		assertEquals(expected, actual);
+	}
 // 3. Return the average from the numbers among a given rank, skip the n first elements ---------
 	@Test
 	void testAverageNumber() {
@@ -52,7 +68,7 @@ class Java8ExamplesTest {
 		int initialRank = 2;
 		int finalRank = 10;
 		int steps = 12; 
-		assertThrows(NullPointerException.class, () -> {
+		assertThrows(NoSuchElementException.class, () -> {
 			java8Examples.averageNumber(initialRank, finalRank, steps);
 		});
 	}
@@ -67,9 +83,8 @@ class Java8ExamplesTest {
 	
 	@Test
 	void testFirstOrderedWordNull() {
-		List<String> words = null;
 		assertThrows(NullPointerException.class, () -> {
-			java8Examples.firstOrderedWord(words);
+			java8Examples.firstOrderedWord(null);
 		});
 	}
 	
@@ -78,7 +93,7 @@ class Java8ExamplesTest {
 		List<String> words = Arrays.asList();
 		assertNull(java8Examples.firstOrderedWord(words));
 	}
-// 5. Return the bigger square from a given decimal numbers array ------------------------------
+// 5. Return the bigger square from a given decimal numbers array -------------------------------
 	@Test
 	void testBiggerDecimalSquare() {
 		Double[] numbers = {3.05, 4.201, 7.66};
@@ -97,9 +112,8 @@ class Java8ExamplesTest {
 	
 	@Test
 	void testBiggerDecimalSquareNull() {
-		Double[] numbers = null;
 		assertThrows(NullPointerException.class, () -> {
-			java8Examples.biggerDecimalSquare(numbers);
+			java8Examples.biggerDecimalSquare(null);
 		});
 	}
 // 6.  Filter by prefix, order and return the words array changed from the given one ------------
@@ -111,40 +125,158 @@ class Java8ExamplesTest {
 		String[] actual = java8Examples.orderedFilteredWords(words, prefix);
 		assertArrayEquals(expected, actual);
 	}
+	
+	@Test
+	void testOrderedFilteredWordsNullPrefix() {
+		String[] words = {"airpod", "biscuit", "airforce", "zelta", "airplane", "java", "airport"};
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.orderedFilteredWords(words, null);
+		});
+	}
+	
+	@Test
+	void testOrderedFilteredWordsNullArray() {
+		String prefix = "abc";
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.orderedFilteredWords(null, prefix);
+		});
+	}
+	
+	@Test
+	void testOrderedFilteredWordsNull() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.orderedFilteredWords(null, null);
+		});
+	}
+	
+	@Test
+	void testOrderedFilteredWordsEmpty() {
+		String[] words = {};
+		String prefix = "";
+		String[] expected = {};
+		String[] actual = java8Examples.orderedFilteredWords(words, prefix);
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testOrderedFilteredWordsEmptyArray() {
+		String[] words = {};
+		String prefix = "abc";
+		String[] expected = {};
+		String[] actual = java8Examples.orderedFilteredWords(words, prefix);
+		assertArrayEquals(expected, actual);
+	}
 // 7. Return the amount of words with more than four letters in a referenced text file ----------
 	@Test
-	void testBiggerThan4() {
-		String uriTxt = "bin/testassets/bands.txt";
-		int expected = 26;
+	void testBiggerThan4() throws IOException {
+		String uriTxt = "testassets/bands.txt";
+		int expected = 27;
 		int actual = java8Examples.biggerThan4(uriTxt);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	void testBiggerThan4NullUri() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.biggerThan4(null);
+		});
+	}
+	
+	@Test
+	void testBiggerThan4InvalidUri() {
+		String uriTxt = "/Documents/non-existent.txt";
+		assertThrows(IOException.class, () -> {
+			java8Examples.biggerThan4(uriTxt);
+		});
+	}
 /* 8. Return from a referenced text file, in uppercase,------------------------------------------
- * 	the list of words that end with a given postfix ---------------------------------------------
+ * 	the list of words that end with a given suffix ---------------------------------------------
  */
 	@Test
-	void testEndWithPostfix() {
-		String uriTxt = "bin/testassets/bands.txt";
-		String postfix = "es";
-		List<String> expected = Arrays.asList("BEATLES", "STONES", "RAMONES");
-		List<String> actual = java8Examples.endWithPostfix(uriTxt, postfix); 
+	void testEndWithSuffix() throws IOException {
+		String uriTxt = "testassets/bands.txt";
+		String suffix = "es";
+		List<String> expected = Arrays.asList("BEATLES", "STONES", "RAMONES", "GALAXIES");
+		List<String> actual = java8Examples.endWithSuffix(uriTxt, suffix); 
 		assertIterableEquals(expected, actual);
+	}
+	
+	@Test
+	void testEndWithSuffixNull() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.endWithSuffix(null, null);
+		});
+	}
+	
+	@Test
+	void testEndWithSuffixNullUri() {
+		String Suffix = "es";
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.endWithSuffix(null, Suffix);
+		});
+	}
+	
+	@Test
+	void testEndWithSuffixNullSuffix() {
+		String uriTxt = "testassets/bands.txt";
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.endWithSuffix(uriTxt, null);
+		});
+	}
+	
+	@Test
+	void testEndWithSuffixInvalidUri() {
+		String uriTxt = "/Documents/non-existent.txt";
+		String Suffix = "es";
+		assertThrows(IOException.class, () -> {
+			java8Examples.endWithSuffix(uriTxt, Suffix);
+		});
 	}
 // 9. Return how many rows with numbers does a referenced CSV file has --------------------------
 	@Test
-	void testRowsWithNumbers() {
-		String uriCsv = "bin/testassets/bands.csv";
-		int expected = 2;
+	void testRowsWithNumbers() throws IOException {
+		String uriCsv = "testassets/bands.csv";
+		int expected = 3;
 		int actual = java8Examples.rowsWithNumbers(uriCsv);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	void testRowsWithNumbersNullUri() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.rowsWithNumbers(null);
+		});
+	}
+	
+	@Test
+	void testRowsWithNumbersInvalidUri() {
+		String uriCsv = "/Documents/non-existent.csv";
+		assertThrows(IOException.class, () -> {
+			java8Examples.rowsWithNumbers(uriCsv);
+		});
+	}
 // 10. Return an array with all prime numbers from a referenced CSV file ------------------------
 	@Test
-	void testPrimeNumbers() {
-		String uriCsv = "bin/testassets/numbers.csv";
-		int[] expected = {2, 7, 3, 11, 105};
+	void testPrimeNumbers() throws IOException {
+		String uriCsv = "testassets/numbers.csv";
+		int[] expected = {2, 7, 3, 11};
 		int[] actual = java8Examples.primeNumbers(uriCsv);
 		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	void testPrimeNumbersNullUri() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.primeNumbers(null);
+		});
+	}
+	
+	@Test
+	void testPrimeNumbersInvalidUri() {
+		String uriCsv = "/Documents/non-existent.csv";
+		assertThrows(IOException.class, () -> {
+			java8Examples.primeNumbers(uriCsv);
+		});
 	}
 /* 11. Return a String with the IntSummaryStatistics from the people's ages in a given Person ---
  * list. Person class should have id, name, age properties. Skip those with repeated id --------- 
@@ -160,5 +292,12 @@ class Java8ExamplesTest {
 		String expected = "IntSummaryStatistics{count=3, sum=56, min=12, average=18,666667, max=23}";
 		String actual = java8Examples.ageStatistics(people);
 		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAgeStatisticsNull() {
+		assertThrows(NullPointerException.class, () -> {
+			java8Examples.ageStatistics(null);
+		});
 	}
 }
